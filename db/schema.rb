ffb40c2208d95a0b7e5cf80912a9a46eb3458cc6
M. Randy Anugerah <89436357+MyPumpkinz@users.noreply.github.com>
2022-06-07 10:12:40 +0000

    add category for book

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_042154) do
+ActiveRecord::Schema.define(version: 2022_06_07_093123) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
