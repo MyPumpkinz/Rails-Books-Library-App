ed9ec07384d61005b2c28dfa088f8a5c01fad2a0
M. Randy Anugerah <89436357+MyPumpkinz@users.noreply.github.com>
2022-06-13 15:32:01 +0000

    add controller reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'search', to: 'search#index'
 
   devise_for :users
-  resources :books
   resources :downloads
+  resources :books do 
+    resources :reviews
+  end
 
   root to: 'books#index'
 end
