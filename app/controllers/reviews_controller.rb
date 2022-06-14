57e9e73b33acd8fad1744d14758adaf61251dae5
M. Randy Anugerah <89436357+MyPumpkinz@users.noreply.github.com>
2022-06-14 13:35:48 +0000

    add bootstrap review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,30 +1,60 @@
 class ReviewsController < ApplicationController
-  before_action :find_book 
-  
-  def new
-    @review = Review.new
-  end
+  before_action :find_book
+	before_action :find_review, only: [:edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :edit]
 
-  def create
-    @review = Review.new(review_params)
-    @review.book_id = @book_id
-    @review.user_id = current._user.id
+	def new
+		@review = Review.new
+	end
 
-    if @review.save
-      redirect_to book_path(book)
+	def create
+		@review = Review.new(review_params)
+		@review.book_id = @book.id
+		@review.user_id = current_user.id
+
+		if @review.save
+			redirect_to download_path(@book)
+		else
+			render 'new'
+		end
+	end
+
+  def show
+    if @books.reviews.blank?
+      @average_review = 0
     else
-      render "new"
+       @average_review = @book.reviews.average(:rating).round(2)
     end
   end
 
-  private
+	def edit
+	end
 
-  def review
-    params.require(:review).permit(:rating, :comment)
-  end
+	def update
+		if @review.update(review_params)
+			redirect_to download_path(@book)
+		else
+			render 'edit'
+		end
+	end
 
-  def find_book
-    @book = Book.find(params[:book_id])
-  end
-  
+	def destroy
+		@review.destroy
+		redirect_to download_path(@book)
+	end
+
+	private
+
+		def review_params
+			params.require(:review).permit(:rating, :comment)
+		end
+
+		def find_book
+			@book = Book.find(params[:book_id])
+		end
+
+		def find_review
+			@review = Review.find(params[:id])
+		end
+
 end
